0f160d23df5144dca02f1dc583f8cf8313286225
Pedro Henrique Alarcon <pedrohenriquealarcon@gmail.com>
2021-11-26 17:39:28 -0300

    CSS MODAS WEBVIEW V1.2.0

---
+++ app/src/main/java/com/alfaumuarama/css_modas_webview/VitrineActivity.java
@@ -0,0 +1,14 @@
+package com.alfaumuarama.css_modas_webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VitrineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vitrine);
+    }
+}
