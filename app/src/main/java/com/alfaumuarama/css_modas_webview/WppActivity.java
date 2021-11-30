22d4ec3ba4f041596ce7a0ca0476944dd0693d1f
Pedro Henrique Alarcon <pedrohenriquealarcon@gmail.com>
2021-11-30 17:48:54 -0300

    App Final

--- app/src/main/java/com/alfaumuarama/css_modas_webview/WppActivity.java
+++ app/src/main/java/com/alfaumuarama/css_modas_webview/WppActivity.java
@@ -2,14 +2,20 @@ package com.alfaumuarama.css_modas_webview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WppActivity extends AppCompatActivity {
 
     WebView wppView;
+    String numero, msg;
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -26,9 +32,32 @@ public class WppActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wpp);
 
         wppView = findViewById(R.id.wppView);
-        wppView.loadUrl("https://api.whatsapp.com/send?phone=%27+item.whatsapp+%27&text=%27");
+        wppView.loadUrl("https://api.whatsapp.com/send?phone=55'+item.whatsapp");
 
         wppView.getSettings().setJavaScriptEnabled(true);
         wppView.setWebViewClient(new WebViewClient());
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        wppView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                super.onReceivedSslError(view, handler, error);
+            }
+        });
     }
 }
